b3d2f90273caa22f16e2808608db604cc7df138d
Shyam Tala <49745830+shyamtala003@users.noreply.github.com>
2024-04-22 05:50:23 +0000

    FEAT : Logout functionality added

--- controllers/auth/v1/logout.controller.js
+++ controllers/auth/v1/logout.controller.js
@@ -1,3 +1,9 @@
+import setResponse from "../../../utils/response.util.js";
+
 export const logout = (req, res) => {
-  res.send("logout");
+  // 1. remove all token cookies
+  res.cookie("refresh token", "", { maxAge: 0 });
+  res.cookie("access token", "", { maxAge: 0 });
+
+  setResponse(res, 200, true, "user logout successful.");
 };
