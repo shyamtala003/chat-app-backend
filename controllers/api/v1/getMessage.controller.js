5302ef01b7c8fe3eaddd87594420d465e627d385
shyamtala-dev <168068213+shyamtala-dev@users.noreply.github.com>
2024-05-02 08:59:02 +0000

    UPDATE : on fetch conversation set the messageread status true

--- controllers/api/v1/getMessage.controller.js
+++ controllers/api/v1/getMessage.controller.js
@@ -1,22 +1,31 @@
 import { conversationModel } from "../../../models/conversation.model.js";
 import setResponse from "../../../utils/response.util.js";
+import { messageModel } from "../../../models/message.model.js";
 
 export default async function getMessage(req, res) {
   try {
-    // 1. prepare search payload
+    // 1. Prepare search payload
     const { toChatUserId } = req.params;
     const senderId = req?.user?._id;
 
-    // 2. find the conversation between the receiver and the sender
+    // 2. Update the read status of messages
+    await messageModel.updateMany(
+      { senderId: toChatUserId, receiverId: senderId, read: false },
+      { $set: { read: true } }
+    );
+
+    // 3. Find the conversation between the receiver and the sender
     const conversation = await conversationModel.getConversation(
       senderId,
       toChatUserId
     );
 
-    return setResponse(res, 200, true, "messages fetched successfully", {
+    return setResponse(res, 200, true, "Messages fetched successfully", {
       messages: conversation ? conversation.messages : [],
+      read: true, // Indicate that messages are now read
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return setResponse(res, 500, false, "Internal server error");
   }
 }
